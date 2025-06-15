9cff006e442981d65f48414c1a6e0b084bc81ced
PedroPerez14 <pjperezTIC@gmail.com>
2025-06-16 01:31:23 +0200

    Completed tutorials for logging module and premake build system

---
+++ premake5.lua
@@ -0,0 +1,118 @@
+workspace "ARC_engine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	-- DELETE LATER
+	startproject "Sandbox"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "ARC_engine"
+	location "ARC_engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ARC_PLATFORM_WINDOWS",
+			"ARC_BUILD_DLL"
+		}
+
+		buildoptions
+		{
+			"/utf-8"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ARC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ARC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ARC_DIST"
+		optimize "On"
+	
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"ARC_engine/vendor/spdlog/include",
+		"ARC_engine/src"
+	}
+
+	links
+	{
+		"ARC_engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ARC_PLATFORM_WINDOWS"
+		}
+
+		buildoptions
+		{
+			"/utf-8"
+		}
+
+	filter "configurations:Debug"
+		defines "ARC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ARC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ARC_DIST"
+		optimize "On"
